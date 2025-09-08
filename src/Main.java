3b44b3c08443d1e3062608e222c5379a9809e81e
127CDietrich <127cdietrich@frhsd.com>
2025-09-08 10:51:50 -0400

    hi

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello my name is Carson!");
         System.out.println("Hi Carson, I'm Brandon");
+        System.out.println("Hi Carson I'm Christian");
     }
 }
